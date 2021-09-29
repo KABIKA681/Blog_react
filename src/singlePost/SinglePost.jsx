62f7cd4e3036dbd8678b960052a5fe1c7a43dbd7
pascalmpang <pascalkabikampan@gmail.com>
2021-09-29 15:48:50 +0200

    commit

--- src/singlePost/SinglePost.jsx
+++ src/singlePost/SinglePost.jsx
@@ -10,7 +10,7 @@ export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = "http://localhost:3004/images/";
+  const PF = "http://localhost:3000/images/";
   const { user } = useContext(Context);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
