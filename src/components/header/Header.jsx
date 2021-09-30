7630c543138e8e1d13076415724bf8b2b2c03b4c
pascalmpang <pascalkabikampan@gmail.com>
2021-09-30 22:37:43 +0200

    working

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <div className="header">
             <div className="headerTitles">
-                <span className="headerTitleSmall">RwandAir</span>
+                <span className="headerTitleSmall">RwandaAir</span>
                 <span className="headerTitleLarge">Book Your Place now</span>
             </div>
             <img className="headerImg" src="https://pbs.twimg.com/media/E-r81stWUAIm8cG?format=jpg&name=large" alt="" />
